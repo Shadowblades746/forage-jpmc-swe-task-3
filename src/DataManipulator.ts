b1a345fc457279f0776d29c394608fbeb40b60a3
Shadowblades746 <77968152+Shadowblades746@users.noreply.github.com>
2023-07-24 19:53:39 +0000

    yes

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,4 +1,5 @@
-import { ServerRespond } from './DataStreamer';
+import {ServerRespond} from './DataStreamer';
+
 
 export interface Row {
   price_abc: number,
@@ -12,17 +13,17 @@ export interface Row {
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]): Row {
+  static generateRow(serverRespond: ServerRespond[]): Row {
     const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) /2;
     const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) /2;
     const ratio = priceABC / priceDEF;
     const upperBound = 1 + 0.1;
     const lowerBound = 1 - 0.1;
     return {
-      price_abc; priceABC,
+      price_abc: priceABC,
       price_def: priceDEF,
       ratio,
-      timestamp: serverRespond[0],timestamp > serverRespond[1].timestamp ?
+      timestamp: serverRespond[0].timestamp > serverRespond[1].timestamp ?
         serverRespond[0].timestamp : serverRespond[1].timestamp,
         upper_bound: upperBound,
         lower_bound: lowerBound,
